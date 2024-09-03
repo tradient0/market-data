735d5d4418825c934eacf2b5611e0411f5b10f9c
cypherhack99 <58860117+cypherhack99@users.noreply.github.com>
2024-09-03 10:59:38 +0530

    dfwedew

--- automation/fetch_cookies.js
+++ automation/fetch_cookies.js
@@ -2,19 +2,18 @@
 
 const axios = require('axios');
 const tough = require('tough-cookie');
-const axiosCookieJarSupport = require('axios-cookiejar-support').default;
+const { wrapper } = require('axios-cookiejar-support');
 
 (async () => {
   // Initialize a cookie jar
   const cookieJar = new tough.CookieJar();
-  axiosCookieJarSupport(axios);
+  
+  // Wrap axios to add cookie jar support
+  const client = wrapper(axios.create({ jar: cookieJar, withCredentials: true }));
 
   try {
     // Make a request with the cookie jar
-    const response = await axios.get('https://www.nseindia.com/companies-listing/corporate-filings-actions', {
-      jar: cookieJar,
-      withCredentials: true,
-    });
+    const response = await client.get('https://www.nseindia.com/companies-listing/corporate-filings-actions');
 
     // Retrieve and format cookies
     const cookies = cookieJar.getCookieStringSync(response.config.url);
